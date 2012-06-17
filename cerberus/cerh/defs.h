e6d340482a91140b3abac152050ba2598f655fd5
Arrogant Penguin <arrogantpenguin@gmail.com>
2012-06-17 10:59:32 -0400

    Added a TODO, defs.h, and template

--- cerberus/cerh/defs.h
+++ cerberus/cerh/defs.h
@@ -5,7 +5,14 @@
 
 /* Arrogant Penguin - 6/17/2012
 
+/* Define the defs.h file to the system */
+#ifndef _DEFS_H_
+#define _DEFS_H_
+
 /* Enable POSIX1.2008 support */
 #define _XOPEN_SOURCE 700
 
-#include <cerh/defs.h>
+
+/* Close the file */
+#endif /* !_DEFS_H_ */
+
