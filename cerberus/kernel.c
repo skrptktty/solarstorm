e5bf9c768e222211ef74e1b9a2d4089bcb1a7973
Arrogant Penguin <arrogantpenguin@gmail.com>
2012-06-17 10:01:39 -0400

    Updated the LICENSE to the Solar Storm License, added kernel.c

---
+++ cerberus/kernel.c
@@ -0,0 +1,9 @@
+/* This is the Cerberus Kernel from Arrogant Penguin.
+   The code is licensed under the Solar Storm License.
+   For the complete text of this license please see
+   the LICENSE file. */
+
+/* Arrogant Penguin - 6/17/2012
+
+/* Enable POSIX1.2008 support */
+#define _XOPEN_SOURCE 700
