0a314e6ff3c9647d72adf430479c49537ea5ab7c
Arrogant Penguin <arrogantpenguin@gmail.com>
2012-06-22 17:42:18 -0400

    Added cerberus/{,include/*, sys/*} and rtems

---
+++ cerberus/sys/select.h
@@ -0,0 +1,6 @@
+/* This is the sys/select.h file, it is written to be 100% POSIX
+   compliant.  This file is covered under the Solar System License
+   for more information please see the LICENSE file under the root
+   directory */
+
+/* Arrogant Penguin - 06222012 */
