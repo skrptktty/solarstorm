cd28ecd8b43922ccf224e79a1bcc02d913406f3b
Arrogant Penguin <arrogantpenguin@gmail.com>
2012-06-23 18:09:02 -0400

    Edited template - added sys/wait.h

---
+++ cerberus/include/sys/wait.h
@@ -0,0 +1,11 @@
+/* This is the sys/wait.h file, it is written to be 100% POSIX
+   compliant.  This file is covered under the Solar System License
+   for more information please see the LICENSE file under the root
+   directory. */
+
+/* Created :: Arrogant Penguin - 06232012 */
+
+#ifndef _WAIT_H
+#define _WAIT_H
+
+#endif /* sys/wait.h */
